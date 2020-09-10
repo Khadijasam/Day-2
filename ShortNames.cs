15fa9330855a8d03d3711eb75fb962350b2cc9a2
Khadijasam <70614192+Khadijasam@users.noreply.github.com>
2020-09-10 07:35:27 +0530

    Add files via upload

---
+++ ShortNames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class ShortNames
+    {
+        public static void FirstMiddle(string str)
+        {
+            string[] nameParts = str.Split();
+            nameParts[0] = nameParts[0].Substring(0, 1);
+            nameParts[1] = nameParts[1].Substring(0, 1);
+            string name = String.Join(" ", nameParts);
+            Console.WriteLine(name);
+            Console.Read();
+        }
+    }
+}
